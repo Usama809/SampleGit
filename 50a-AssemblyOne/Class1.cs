9cdc15134184158ef00f15da3e8d30d6db08bc09
Usama Bin Amjad <62556162+Usama809@users.noreply.github.com>
2020-03-24 02:34:37 +0500

    50a-Assembly One Csharp

---
+++ 50a-AssemblyOne/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AssemblyOne
+{
+    public class AssemblyOneClassI
+    {
+        protected internal int ID = 101;
+
+    }
+    public class AssemblyOneClassII : AssemblyOneClassI
+    {
+        public void SampleMethode()
+        {
+            AssemblyOneClassI AI = new AssemblyOneClassI();
+            Console.WriteLine(AI.ID);
+        }
+    }
+}
