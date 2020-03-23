67503333d55ef034554046528fbc4d31ec9bdd90
Usama Bin Amjad <62556162+Usama809@users.noreply.github.com>
2020-03-24 02:35:37 +0500

    50b-Assembly Two Csharp

---
+++ 50b-AssemblyTwo/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AssemblyOne;
+
+namespace AssemblyTwo 
+{
+    public class AssemblyTwoClassI : AssemblyOneClassI
+    { 
+        public void print()
+        {
+            AssemblyOneClassI A1 = new AssemblyOneClassI();
+            base.ID = 101;
+
+            AssemblyTwoClassI A2 = new AssemblyTwoClassI();
+            A2.ID = 102;
+        }
+
+    }
+}
